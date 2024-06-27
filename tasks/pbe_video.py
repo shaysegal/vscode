718ffb54b13cddf88bab2b2275f6df97326a1a90
shaysegal <siglons@gmail.com>
2024-06-27 12:44:38 +0300

    pbe_video

---
+++ tasks/pbe_video.py
@@ -0,0 +1,46 @@
+
+def double_number(x):
+    result = x * 2
+    return result 
+
+print(double_number(2))
+print(double_number(5))
+
+def triple_number(x):
+    result = ??
+    return result
+
+print(triple_number(1))
+print(triple_number(2))
+print(triple_number(3))
+
+def mod2(x):
+    result = ??
+    return result
+print(mod2(1))
+print(mod2(3))
+print(mod2(5))
+print(mod2(4))
+
+def squre_area(x,y):
+    result = ??
+    return result
+
+print(squre_area(2,2))
+print(squre_area(5,5))
+
+increment = 3
+def example_program(x):
+    result = ??
+    return result
+print(example_program(1)) # out 4
+print(example_program(2)) # out 5
+print(example_program(3)) # out 6
+
+def triple_number_wrong(x):
+    result = ??
+    return result
+
+print(triple_number_wrong(1)) #3
+print(triple_number_wrong(2)) #7 -> mistake
+print(triple_number_wrong(3)) #9
